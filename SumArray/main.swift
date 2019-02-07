d8669b347751f55ddc80a257adcd57aa79781dc0
Jason Harrison <jasonharrisonvancouver@gmail.com>
2019-02-07 13:51:39 -0800

    works, but tests somehow say it doesn't

--- SumArray/main.swift
+++ SumArray/main.swift
@@ -1,5 +1,5 @@
-//  Copyright (c) 2017 Lighthouse Labs. All rights reserved.
-//
+// Copyright (c) 2017 Lighthouse Labs. All rights reserved.
+// 
 // Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 // distribute, sublicense, create a derivative work, and/or sell copies of the
 // Software in any work that is designed, intended, or marketed for pedagogical or
@@ -7,7 +7,7 @@
 // or information technology.  Permission for such use, copying, modification,
 // merger, publication, distribution, sublicensing, creation of derivative works,
 // or sale is expressly withheld.
-//
+// 
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -17,45 +17,10 @@
 // THE SOFTWARE.
 
 import Foundation
-
-struct Sums {
-  func sumTheItems(_ array: [Any]) -> Int {
-    // Sum all numbers in the array
-    return -1
-  }
-  
-  
-  
-  
-  
-  func sumItems(_ array: [Any]) -> Int {
-    for item in array{
-      //print("num elements in the top array \(array) is \(array.count)")
-      if let innerArray = item as? [Any] {
-       //  print("num items in inner array \(innerArray) is \(innerArray.count)")
-        var a = array
-        a.removeFirst()
-         return sumItems(innerArray) + sumItems(a)
-      } else {
-        var arr = array
-        arr.removeFirst()
-     //   print("going to add \(item)")
-        return (item as! Int) + sumItems(arr)//
-      }
-    }
-    return 0
-  }
-  
-  
-  
- 
-
-  
-  func printSum(a: [Any]){
-    print(sumItems(a))
-  }
-  
-}
-
-
-
+var sums: Sums = Sums()
+var a: [Any] = [1, [2, 3], [4, 5, 6], 7]
+sums.printSum(a: [1, 1])
+sums.printSum(a: [5, 6, [7, 8]])
+sums.printSum(a: [1, [2, 3, 4], [5, 6], [7, 8]])
+sums.printSum(a: [[[5], [[[4]]], [[3], 2]], 1, []])
+sums.printSum(a: [1, [3, 3], [5, 5]])
